73428b87721007347f1b977f407c950f31419f73
moaath ibrahim <moaaz@gmail.com>
2024-10-09 15:12:43 -0700

    Add buttons

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-alert("Hello from file in git hub")
+const button = document.querySelector("button");
+const newButton = document.querySelector(".dont");
+
+button.addEventListener('click', function(e){
+    alert("You clicked the button");
+})
+
+newButton.addEventListener('click', function(e){
+    alert("I told you don't click it!")
+})
